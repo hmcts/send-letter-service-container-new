79c81fd183b347e168d3ae02c54c3bd4994a51e2
rishisharma <rishi.sharma@hmcts.net>
2021-04-29 15:45:33 +0100

    DTSBSP:429: blob reader

--- src/main/java/uk/gov/hmcts/reform/sendletter/blob/component/BlobReader.java
+++ src/main/java/uk/gov/hmcts/reform/sendletter/blob/component/BlobReader.java
@@ -3,20 +3,18 @@ package uk.gov.hmcts.reform.sendletter.blob.component;
 import com.azure.storage.blob.BlobContainerClient;
 import com.azure.storage.blob.BlobServiceClient;
 import com.azure.storage.blob.BlobServiceClientBuilder;
-import com.azure.storage.blob.models.BlobItem;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import java.util.Optional;
-
 @Component
 public class BlobReader {
+
     private static final Logger LOG = LoggerFactory.getLogger(BlobReader.class);
     private static final String NEW_CONTAINER = "new";
     private final String connection;
-    private String blobName;
+
 
     public BlobReader(@Value("${storage.connection}") String connection) {
         this.connection = connection;
@@ -31,12 +29,8 @@ public class BlobReader {
         BlobServiceClient blobServiceClient = new BlobServiceClientBuilder().connectionString(connection).buildClient();
         BlobContainerClient containerClient = blobServiceClient.getBlobContainerClient(NEW_CONTAINER);
 
-        Optional<BlobItem> first = containerClient.listBlobs().stream().findFirst();
-        if (first.isPresent()) {
-            blobName = first.get().getName();
-        }
+        String blobName = containerClient.listBlobs().stream().findFirst().map(blob -> blob.getName()).orElse("");
         LOG.info("BlobReader:: Blob name: {}", blobName);
-
         return blobName;
     }
 }
--- src/test/java/uk/gov/hmcts/reform/sendletter/blob/BlobProcessorTest.java
+++ src/test/java/uk/gov/hmcts/reform/sendletter/blob/BlobProcessorTest.java
@@ -2,14 +2,20 @@ package uk.gov.hmcts.reform.sendletter.blob;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.hmcts.reform.sendletter.blob.component.BlobReader;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 class BlobProcessorTest {
     private BlobProcessor processBlob;
+
+    @Mock
     private BlobReader blobReader;
 
     @BeforeEach
