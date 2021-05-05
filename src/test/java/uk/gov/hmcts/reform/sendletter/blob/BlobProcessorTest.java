4b9be77afee456b22f5babdabd101eb4ed35a75b
rishikrsharma <rishikrsharma@users.noreply.github.com>
2021-05-05 11:52:25 +0100

    DTSBPS-523: Stitching function - copy original blob to backup container (#13)

--- src/test/java/uk/gov/hmcts/reform/sendletter/blob/BlobProcessorTest.java
+++ src/test/java/uk/gov/hmcts/reform/sendletter/blob/BlobProcessorTest.java
@@ -5,9 +5,11 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import uk.gov.hmcts.reform.sendletter.blob.component.BlobBackup;
 import uk.gov.hmcts.reform.sendletter.blob.component.BlobReader;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -17,17 +19,23 @@ class BlobProcessorTest {
 
     @Mock
     private BlobReader blobReader;
+    @Mock
+    private BlobBackup blobBackup;
 
     @BeforeEach
     void setUp() {
-        processBlob = new BlobProcessor(blobReader);
+        processBlob = new BlobProcessor(blobReader, blobBackup);
         when(blobReader.retrieveFileToProcess()).thenReturn("blob.txt");
+        when(blobBackup.backupBlob(anyString())).thenReturn("copyId");
     }
 
     @Test
     void should_process_blob_when_triggered() throws InterruptedException {
         boolean processed = processBlob.read();
         verify(blobReader).retrieveFileToProcess();
+        String copiedBlob = blobBackup.backupBlob(blobReader.retrieveFileToProcess());
+
         assertThat(processed).isTrue();
+        assertThat(copiedBlob).isEqualTo("copyId");
     }
 }
