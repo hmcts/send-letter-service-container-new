72341378a77809692143c25583071ef4c65f72b3
rishisharma <rishi.sharma@hmcts.net>
2021-05-05 08:33:25 +0100

    DTSBPS-523: Stitching function - copy original blob to backup container

--- src/main/java/uk/gov/hmcts/reform/sendletter/blob/BlobProcessor.java
+++ src/main/java/uk/gov/hmcts/reform/sendletter/blob/BlobProcessor.java
@@ -3,19 +3,24 @@ package uk.gov.hmcts.reform.sendletter.blob;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import uk.gov.hmcts.reform.sendletter.blob.component.BlobBackup;
 import uk.gov.hmcts.reform.sendletter.blob.component.BlobReader;
 
 @Service
 public class BlobProcessor {
     private static final Logger LOG = LoggerFactory.getLogger(BlobProcessor.class);
-    private final BlobReader blobReader;
 
-    public BlobProcessor(BlobReader blobReader) {
+    private final BlobReader blobReader;
+    private final BlobBackup blobBackup;
+
+    public BlobProcessor(BlobReader blobReader, BlobBackup blobBackup) {
         this.blobReader = blobReader;
+        this.blobBackup = blobBackup;
     }
 
     public boolean read() throws InterruptedException {
         String blob = blobReader.retrieveFileToProcess();
+        blobBackup.backupBlob(blob);
         LOG.info("BlobName : {}", blob);
         Thread.sleep(10_000);
         return true;
