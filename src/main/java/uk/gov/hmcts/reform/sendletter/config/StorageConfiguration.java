9b8c6ca83b3cce564ff0f9c0c2eee57858c020a3
rishikrsharma <rishikrsharma@users.noreply.github.com>
2021-05-12 11:41:40 +0100

    DTSBPS-517: Reader and backup blobs (#27)

--- src/main/java/uk/gov/hmcts/reform/sendletter/config/StorageConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/sendletter/config/StorageConfiguration.java
@@ -2,6 +2,7 @@ package uk.gov.hmcts.reform.sendletter.config;
 
 import com.azure.storage.blob.BlobServiceClient;
 import com.azure.storage.blob.BlobServiceClientBuilder;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,5 +16,10 @@ public class StorageConfiguration {
         return new BlobServiceClientBuilder()
                 .connectionString(connection)
                 .buildClient();
+    }
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper();
     }
 }
