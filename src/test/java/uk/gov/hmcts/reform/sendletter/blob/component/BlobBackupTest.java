db6858d5a9099bf1aa28b41e88e62264a605f817
rishisharma <rishi.sharma@hmcts.net>
2021-05-21 14:01:56 +0100

    DTSBPS-588: Fix - delete blob order

--- src/test/java/uk/gov/hmcts/reform/sendletter/blob/component/BlobBackupTest.java
+++ src/test/java/uk/gov/hmcts/reform/sendletter/blob/component/BlobBackupTest.java
@@ -17,7 +17,6 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.hmcts.reform.sendletter.config.AccessTokenProperties;
 import uk.gov.hmcts.reform.sendletter.model.in.ManifestBlobInfo;
 import uk.gov.hmcts.reform.sendletter.model.in.PrintResponse;
-import uk.gov.hmcts.reform.sendletter.model.in.PrintStatus;
 import uk.gov.hmcts.reform.sendletter.services.SasTokenGeneratorService;
 
 import java.io.FileWriter;
@@ -26,13 +25,15 @@ import java.io.IOException;
 import static com.google.common.base.Charsets.UTF_8;
 import static com.google.common.io.Resources.getResource;
 import static java.util.Collections.singletonList;
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.lenient;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 class BlobBackupTest {
@@ -53,14 +54,14 @@ class BlobBackupTest {
     @Mock
     private BlobClient client;
 
-    private BlobClient sourceBlobClient;
     private BlobBackup blobBackup;
     private ManifestBlobInfo blobInfo;
-    private ObjectMapper mapper;
     private String sasToken;
+    private ObjectMapper mapper;
+    private String json;
 
     @BeforeEach
-    void setUp() {
+    void setUp() throws IOException {
         mapper = new ObjectMapper();
         given(blobManager.getAccountUrl()).willReturn("http://test.account");
 
@@ -81,7 +82,7 @@ class BlobBackupTest {
 
         sasToken = sasTokenGeneratorService.generateSasToken(TEST_SERVICE_NAME);
 
-        sourceBlobClient = new BlobClientBuilder()
+        var sourceBlobClient = new BlobClientBuilder()
                 .endpoint(blobManager.getAccountUrl())
                 .sasToken(sasToken)
                 .containerName(NEW_CONTAINER)
@@ -89,24 +90,12 @@ class BlobBackupTest {
                 .buildClient();
 
         blobInfo = new ManifestBlobInfo(TEST_SERVICE_NAME, NEW_CONTAINER, BLOB_NAME);
-    }
-
-    @Test
-    void should_copy_blob_with_path_from_new_container_to_backup_container() throws IOException {
-
         given(blobManager.getContainerClient(BACKUP_CONTAINER)).willReturn(destContainerClient);
-        given(destBlobClient
-                .copyFromUrl("33dffc2f-94e0-4584-a973-cc56849ecc0b-sscs-SSC001-mypdf.pdf"
-                        + "?" + anyString())).willReturn("id");
-        given(destBlobClient
-                .copyFromUrl("33dffc2f-94e0-4584-a973-cc56849ecc0b-sscs-SSC001-2.pdf"
-                        + "?" + anyString())).willReturn("id");
-        given(destBlobClient.copyFromUrl(sourceBlobClient.getBlobUrl() + "?" + anyString())).willReturn("id");
         given(destContainerClient.getBlobClient(anyString())).willReturn(destBlobClient);
-
         given(blobManager.getBlobClient(any(), any(), any())).willReturn(client);
+
         mapper = spy(mapper);
-        String json = Resources.toString(getResource("print_job_response.json"), UTF_8);
+        json = Resources.toString(getResource("print_job_response.json"), UTF_8);
         mapper.registerModule(new JavaTimeModule());
         PrintResponse printResponse = mock(PrintResponse.class);
 
@@ -115,15 +104,19 @@ class BlobBackupTest {
         myWriter.close();
 
         lenient().when(mapper.readValue(json, PrintResponse.class)).thenReturn(printResponse);
-
-        PrintResponse response = blobBackup.backupBlobs(blobInfo);
-        assertThat(response.printJob.printStatus).isSameAs(PrintStatus.PROCESSED);
     }
 
     @Test
-    void should_copy_blob_without_path_from_new_container_to_backup_container() throws IOException {
+    void should_copy_blob_with_path_from_new_container_to_backup_container() {
+        var response = blobBackup.backupBlobs(blobInfo);
+        assertNotNull(response);
+        verify(destBlobClient, times(3)).copyFromUrl(any());
+    }
+
+    @Test
+    void should_copy_blob_without_path_from_new_container_to_backup_container() {
         String blobName = "print_job_response.json";
-        sourceBlobClient = new BlobClientBuilder()
+        var sourceBlobClient = new BlobClientBuilder()
                 .endpoint(blobManager.getAccountUrl())
                 .sasToken(sasToken)
                 .containerName(NEW_CONTAINER)
@@ -132,30 +125,9 @@ class BlobBackupTest {
 
         blobInfo = new ManifestBlobInfo(TEST_SERVICE_NAME, NEW_CONTAINER, blobName);
 
-        given(blobManager.getContainerClient(BACKUP_CONTAINER)).willReturn(destContainerClient);
-        given(destBlobClient
-                .copyFromUrl("33dffc2f-94e0-4584-a973-cc56849ecc0b-sscs-SSC001-mypdf.pdf"
-                        + "?" + anyString())).willReturn("id");
-        given(destBlobClient
-                .copyFromUrl("33dffc2f-94e0-4584-a973-cc56849ecc0b-sscs-SSC001-2.pdf"
-                        + "?" + anyString())).willReturn("id");
-        given(destBlobClient.copyFromUrl(sourceBlobClient.getBlobUrl() + "?" + anyString())).willReturn("id");
-        given(destContainerClient.getBlobClient(anyString())).willReturn(destBlobClient);
-
-        given(blobManager.getBlobClient(any(), any(), any())).willReturn(client);
-        mapper = spy(mapper);
-        String json = Resources.toString(getResource("print_job_response.json"), UTF_8);
-        mapper.registerModule(new JavaTimeModule());
-        PrintResponse printResponse = mock(PrintResponse.class);
-
-        FileWriter myWriter = new FileWriter("/var/tmp/print_job_response.json");
-        myWriter.write(json);
-        myWriter.close();
-
-        lenient().when(mapper.readValue(json, PrintResponse.class)).thenReturn(printResponse);
-
-        PrintResponse response = blobBackup.backupBlobs(blobInfo);
-        assertThat(response.printJob.printStatus).isSameAs(PrintStatus.PROCESSED);
+        var response = blobBackup.backupBlobs(blobInfo);
+        assertNotNull(response);
+        verify(destBlobClient, times(3)).copyFromUrl(any());
     }
 
     private void createAccessTokenConfig() {
